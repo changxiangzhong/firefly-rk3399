93acb343187eb79282f891ac9fe2f5f4677937ec
Chang Xiangzhong <xiangzhong.chang@telia.no>
2026-06-23 13:48:08 +0200

    Add rootfs

---
+++ rootfs/build-rootfs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+TARGET=./rootfs-debian
+ARCH=arm
+RELEASE=bookworm
+
+mkdir -p $TARGET
+
+debootstrap --foreign --arch=$ARCH $RELEASE $TARGET http://deb.debian.org/debian
+# 为了在 x86 主机上 chroot 进入 arm64 文件系统，需要拷贝 QEMU
+cp /usr/bin/qemu-arm-static $TARGET/usr/bin/
+update-binfmts --enable qemu-arm
+#
+chroot $TARGET /debootstrap/debootstrap --second-stage
+
